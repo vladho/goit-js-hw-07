2f5139f7c5cd6e50b66fb0ec1a8d19943d0afaee
Vlad <vladhopilo@gmail.com>
2021-01-11 10:19:57 +0200

    upd

---
+++ js/task-4.js
@@ -0,0 +1,42 @@
+//option - 1
+
+// let counterValueRed = document.querySelector("#value")
+// const counterRef = document.querySelector("#counter");
+// let counter = 0;
+
+// const counterFunction = event => {
+//     const decrementRef = event.target.dataset.action;
+//     if ('decrement' === decrementRef) {
+//         counter -= 1;
+//         counterValueRed.innerHTML = counter
+//         }
+//     if ('increment' === decrementRef) {
+//         counter += 1;
+//         counterValueRed.innerHTML = counter
+//     }
+    
+// }
+
+// counterRef.addEventListener("click", counterFunction)
+
+//option - 2
+
+let counter = 0;
+
+const counterValueRed = document.querySelector("#value")
+
+const incrementRef = document.querySelector('button[data-action="increment"]')
+const decrementRef = document.querySelector('button[data-action="decrement"]')
+
+
+incrementRef.addEventListener("click", event => {
+    counter += 1;
+    counterValueRed.innerHTML =  counter
+}
+)
+
+decrementRef.addEventListener("click", event => {
+    counter -= 1;
+    counterValueRed.innerHTML =  counter
+}
+)
---
+++ js/task-5.js
@@ -0,0 +1,13 @@
+const nameInputRef = document.querySelector("#name-input")
+const nameOutputRef = document.querySelector("#name-output")
+
+const handleName = event => {
+    if (event.inputType === "insertText") {
+        nameOutputRef.textContent = event.target.value
+    } else
+         nameOutputRef.textContent = "незнакомец"
+}
+
+nameInputRef.addEventListener("input", handleName)
+
+
